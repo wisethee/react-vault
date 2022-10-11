1648d413804bda625b620f2b56a318dbaf63cbcd
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2022-10-11 14:32:36 +0100

    build: remove nx component

--- apps/vault/src/app/app.tsx
+++ apps/vault/src/app/app.tsx
@@ -1,14 +1,5 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-import NxWelcome from './nx-welcome';
-
-export function App() {
-  return (
-    <>
-      <NxWelcome title="vault" />
-      <div />
-    </>
-  );
-}
+const App = () => {
+  return <div>app works!</div>;
+};
 
 export default App;
